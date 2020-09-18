be8c7fbcc1dcd445eafc406594d501435da0f8c1
avrg98 <49884969+avrg98@users.noreply.github.com>
2020-09-18 16:12:08 -0600

    knew code

--- CommingSoon/views.py
+++ CommingSoon/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
+from os.path import exists
 
+class IndexView(TemplateView):
 
-def index(request):
-    return HttpResponse("Hello, world. More comming up")
+    def get_template_names(self):
+        template_name = self.kwargs.get('template','index.html')
+        if not exists('templates/' + template_name):
+            template_name = 'missing.html'
+        return template_name
+
+    
