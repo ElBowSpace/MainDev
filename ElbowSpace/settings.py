45ebb3fdb85b092fed9272e3a3f66a844e1890de
KevinBRitter <ritter_kevin_b@hotmail.com>
2020-09-24 09:47:42 -0600

    minor fix
    spelling
    add settings to git ignore to block passwords

--- ElbowSpace/settings.py
+++ ElbowSpace/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'ElbowSpace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LocalElBowDBS',
+        'USER': 'kevinritter',
+        'PASSWORD': '4dmin4S3rv3r',
+        'port': '3306',
     }
 }
 
