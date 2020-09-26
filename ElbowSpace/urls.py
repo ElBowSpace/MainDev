44c071465ea2df2e4242dba7b56eae524adf442d
KevinBRitter <ritter_kevin_b@hotmail.com>
2020-09-26 15:21:58 -0600

    rename directory
    turned "Comingsoon" to "MainApp"

--- ElbowSpace/urls.py
+++ ElbowSpace/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('ComingSoon.urls')),
+    path('', include('MainApp.urls')),
     path('admin/', admin.site.urls),
 ]
