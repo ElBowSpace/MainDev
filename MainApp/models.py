b46f35b8f611c91da64e75c4ed405c2b973a29ba
KevinBRitter <ritter_kevin_b@hotmail.com>
2020-09-26 16:13:54 -0600

    edited for model clarity
    running the migrations and runserver created tables in the database as expected.

--- MainApp/models.py
+++ MainApp/models.py
@@ -5,6 +5,7 @@ from django.db import models
 # See https://docs.djangoproject.com/en/2.1/topics/db/models/ for details
 class User(models.Model):
     id = models.AutoField(primary_key=True)  # <-- this is included by default in all models
+    connection = models.ManyToManyField('self')
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.CharField(max_length=50)
@@ -15,24 +16,13 @@ class User(models.Model):
         return str(self.id) + " " + self.first_name + " " + self.last_name
 
 
-class Connection(models.Model):
-    user1 = models.ForeignKey(User.id, primary_key=True, on_delete=models.CASCADE)
-    user2 = models.ForeignKey(User.id, primary_key=True, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return str(self.user1) + str(self.user2)
-
-
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
+    reply = models.ForeignKey('self', on_delete=models.CASCADE)
     body = models.CharField(max_length=255)
     time_stamp = models.DateTimeField()
     image = models.TextField(max_length=255)
-    user = models.ForeignKey(User.id, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    class Meta:
-        abstract = True
-
-
-class Comment(Post):
-    post = models.ForeignKey(Post.id, on_delete=models.CASCADE)
+    def __str__(self):
+        return "Post:" + str(self.id) + ", User: " + str(self.user) + ", Reply to: " + str(self.reply)
