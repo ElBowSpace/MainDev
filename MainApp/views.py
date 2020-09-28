77f0fb4ecb786d9b17efaa640acab463d736de3c
Ben McEwen <bdmcewen.bm@gmail.com>
2020-09-28 13:08:07 -0600

    Update views.py

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 from os.path import exists
+from .models import Post
 
 
 class IndexView(TemplateView):
@@ -9,3 +10,31 @@ class IndexView(TemplateView):
         if not exists('templates/' + template_name):
             template_name = 'missing.html'
         return template_name
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class PostDeleteView(DeleteView): 
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
