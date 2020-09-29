b3a1c1521d9724546586c42e60d3704e73edcd94
avrg98 <49884969+avrg98@users.noreply.github.com>
2020-09-29 11:13:01 -0600

    new test code

--- MainApp/tests.py
+++ MainApp/tests.py
@@ -2,12 +2,24 @@ from django.test import SimpleTestCase
 
 class ViewTests(SimpleTestCase):
 
-    def test_fail(self):
-        response = self.client.get('hello')
-        self.assertEqual(response.status_code, 200)
-    
-    def test_view_index(self):
-        response = self.client.get('')
-        self.assertEqual(response.status_code, 200)
+    def check_template(self, page, template):
+        response = self.client.get(page)
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,template_name=template)
 
-# Create your tests here.
+    def test_view(self):
+        self.check_template('/', 'index.html')
+
+    def test_view_prototype(self):
+        self.check_template('/register.html', 'register.html')
+        self.check_template('/login.html', 'login.html')
+        self.check_template('/post_delete.html', 'post_delete.html')
+        self.check_template('/post_detail.html', 'post_detail.html')
+        self.check_template('/post_edit.html', 'post_edit.html')
+        self.check_template('/base.html', 'base.html')
+        self.check_template('/logout.html', 'logout.html')
+        self.check_template('/post_list.html', 'post_list.html')
+        self.check_template('/post_new.html', 'post_new.html')
+
+
+
