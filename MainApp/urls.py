1a204d797cb35bb816a19850a7a2e96ef31f5cce
KevinBRitter <ritter_kevin_b@hotmail.com>
2020-10-01 13:01:25 -0600

    test error correction
    fixed post_detail error in templates.

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('<str:template>', IndexView.as_view()),
     path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     path('new/', PostCreateView.as_view(), name='post_new'),
-    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('<int:pk>/detail/', PostDetailView.as_view(), name='post_detail'),
     path('<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
     path('', PostListView.as_view(), name='post_list'),
     # path('<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
