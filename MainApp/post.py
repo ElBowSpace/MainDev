fa744403481a211c9fa241598df834defb1780db
Ben McEwen <bdmcewen.bm@gmail.com>
2020-10-15 11:33:41 -0600

    Update post.py

--- MainApp/post.py
+++ MainApp/post.py
@@ -1,7 +1,11 @@
 from .models import Post
 
-def add_post(body, time_stamp, user, reply = None, image = None):
-    new_post = Post.objects.create(reply=reply, body= body,time_stamp= time_stamp,image = image, user = user)
+
+def add_post(body, time_stamp, user, reply=None, image=None):
+    new_post = Post.objects.create(reply=reply,
+                                   body=body,
+                                   time_stamp=time_stamp,
+                                   image=image,
+                                   user=user)
     new_post.save()
     return new_post
-
