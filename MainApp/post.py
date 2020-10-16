a6d62a41e22dc0740f08158306fad5644018c30e
Ben McEwen <bdmcewen.bm@gmail.com>
2020-10-15 19:02:31 -0600

    Update post.py

--- MainApp/post.py
+++ MainApp/post.py
@@ -9,3 +9,19 @@ def add_post(body, time_stamp, user, reply=None, image=None):
                                    user=user)
     new_post.save()
     return new_post
+
+
+def edit_post(post_id, new_body=None):
+    revised_post = Post.objects.get(id=post_id)
+    if new_body is not None:
+        revised_post.body = new_body
+    revised_post.save()
+
+def get_post(post_id):
+    return Post.objects.get(id=post_id)
+
+def get_all_posts():
+    return Post.objects.all()
+
+def delete_post(post_id):
+    return Post.objects.get(id=post_id).delete()
