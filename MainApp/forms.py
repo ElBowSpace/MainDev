d862f5b359eff05b19830d1ffc8ba0d0770bd2e5
KevinBRitter <ritter_kevin_b@hotmail.com>
2020-10-24 15:58:43 -0600

    Update forms.py
    Adding edit user meta for User and new / edit form meta for Post

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import User, Post
 
 
+# ----------------------------------------------------------------
+# User Forms
 class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
@@ -12,3 +14,23 @@ class LoginForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('email', 'password',)
+
+
+class EditUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'password',)
+
+
+# ----------------------------------------------------------------
+# Post Forms
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('body',)
+
+
+class EditPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('body',)
