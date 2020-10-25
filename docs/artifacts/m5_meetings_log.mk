58d90b72dcca76b3a34f2312d1c4b2c5872b8fe2
SProv7615 <56846258+SProv7615@users.noreply.github.com>
2020-10-25 11:42:16 -0600

    Create m5_meetings_log.mk

---
+++ docs/artifacts/m5_meetings_log.mk
@@ -0,0 +1,39 @@
+# Log of ElBow Space Group Meetings for Milestone 4
+
+## Format:
+### Date: MM/DD/YY
+### Participants: 
+* role - party 1
+* role - party 2
+### Topics Discussed:
+* topic 1
+* topic 2
+* topic 3
+
+### Date: 10/25/20
+### Participants: 
+* Senior Code Developer - Stephen Provost
+* Project Manager, Overall Code Guru, and Overlord - Kevin Ritter
+### Topics Discussed:
+* Worked on post_detail, post_edit. 
+* Fixed up some code using paired programming.
+
+### Date: 10/24/20
+### Participants: 
+* Senior Code Developer - Stephen Provost
+* Project Manager, Overall Code Guru, and Overlord - Kevin Ritter
+* Head Design Engineer - Angel Rivera
+### Topics Discussed:
+* Discussed Django in detail, and our past coding on the project.
+* Worked on user_detail, user_edit, post_detail, and post_edit for several hours.
+* Encountered several problems that were pushed til tomorrow.
+
+### Date: 10/22/20
+### Participants: 
+* Senior Code Developer - Stephen Provost
+* Project Manager, Overall Code Guru, and Overlord - Kevin Ritter
+* Head Design Engineer - Angel Rivera
+* Head Tester - Ben McEwen
+### Topics Discussed:
+* Discussed Milestone 4. Determined to push M4 backlog into M5.
+* Discussed intentions for M5.
