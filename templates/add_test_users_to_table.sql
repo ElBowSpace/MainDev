03b1aa6b6e23e208bf9d5cceca43d62d1d7e8d62
KevinBRitter <ritter_kevin_b@hotmail.com>
2020-11-30 20:05:59 -0700

    Removing bloat
    templates not used

--- templates/add_test_users_to_table.sql
+++
@@ -1,6 +0,0 @@
-USE local_elbowspace;
-INSERT INTO `users` (FirstName, LastName, Email, Password)
-VALUES ('User1', 'ULast', 'test@local.com', '1234abcd'), 
-('User2','U2Last','test2@local.com','AbCd1234')
-;
-COMMIT;
